15fbd8152b29bc38ce00cc8fa17e2f28072422b9
neerajnakka <neerajnakka8@gmail.com>
2024-05-03 18:08:01 +0530

    data-types

--- src/index.ts
+++ src/index.ts
@@ -63,3 +63,83 @@ function calculateTax1(income: number = 2022): number {
 calculateTax(10); //if passed more or less ts throws error but in js it does not care even if we pass more or less
 //useful config for  functions
 // noUnusedLocals: true,noImplicitReturns:true, noUnusedParameters:true
+
+//Objects in ts
+//For objects we pass object as a type and assign types to the keys in the object
+let employee: { id: number; name: string } = { id: 1, name: 'neeraj' };
+//it best to include all the keys in the object if missed it might give error ex :let employee: { id: number; name: string } = { id: 1};
+//instead do optional let employee: { id: number; name?: string } = { id: 1 }; but remember the value with key optional is undefined
+employee.id = 2;
+//now if you want to make the values in object read only and not changable later on we can use readonly keyword
+let employee1: { readonly id: number; name: string } = {
+  id: 1,
+  name: 'neeraj',
+};
+// employee1.id = 2; //it is not possible it throws error
+let employee2: { id: number; name: string; retire: (date: Date) => void } = {
+  id: 1,
+  name: 'neeraj',
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+//type alias in ts: it is used to create a reusable type and assign it to a variable or an object or an array or a function and use it in the code as a type alias
+//type makes it easy to work with objects or arrays
+//first we create type and declare all keys with types in it   and we pass  the type in the object as its type
+type Employee = {
+  id: number;
+  name: string;
+  retire: (date: Date) => void; // here we pass a function and with its return type
+};
+//now pass the type alias as type to the object
+let employee3: Employee = { id: 1, name: 'neeraj', retire: (date) => {} };
+//this way it simplifies the code of writing objects
+
+//=>Union types in ts
+//union type is used to assign more than one type to a variable
+let id5: number | string = 1;
+id5 = 'hello';
+
+function kgToLbs(weight: number | string): number {
+  //Narrowing : a conversion that is done automatically in ts it displays the methods based on its use like number methods to use if it is number
+  if (typeof weight === 'number') {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+kgToLbs(10);
+kgToLbs('10kg');
+
+//=> Intersection type: used to assign multiple types to a variable and use them in the code as intersection
+type Draggable = {
+  drag: () => void;
+};
+type Resizable = {
+  resize: () => void;
+};
+//now two types are combined together and used in the code
+type UIWidget = Draggable & Resizable;
+//the now using the intersection type should implement all the methods in both the types
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+//=> Extending types: using intersection type to extend an existing type
+type Employee2 = {
+  id: number;
+  name: string;
+};
+type Manager = Employee2 & {
+  project: string;
+};
+let company: Manager = { id: 1, name: 'neeraj', project: 'typescript' };
+
+//=>Literal Types: used to assign specific values to a variable and use
+//literal type is used to limit the values that can be assigned to a variable or the specified values should only be assigned
+let l1: 50 = 50; // it says that only 50 literal should be assigned to l1
+let l2: 50 | 100 = 100; //here l2 can be 50 or 100
+type name = 'neeraj' | 'chandra'; //literal
+let l3: name = 'neeraj'; // here name can be only neeraj or chandra
