dae9751f053c9bb1eeb3d0fcb2d296161e8544e3
neerajnakka <neerajnakka8@gmail.com>
2024-05-03 12:12:36 +0530

    commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assign types to data types
+let x = 10;
+let y = 'hello';
+let z = false;
+let z1 = null;
+// x = 'not a number'; // throws error as string cant be assigned to number
+let z2 = 5;
+z2 = 'string';
+z2 = true;
+//any is a special data type in ts that can be replaced with any other data type value
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//Assign types to data types
+let x: number = 10;
+let y: string = 'hello';
+let z: boolean = false;
+let z1: null = null;
+// x = 'not a number'; // throws error as string cant be assigned to number
+let z2: any = 5;
+z2 = 'string';
+z2 = true;
+//any is a special data type in ts that can be replaced with any other data type value
