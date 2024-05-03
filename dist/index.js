b58d466166170d19ce54cfd60447a34526b5cd00
neerajnakka <neerajnakka8@gmail.com>
2024-05-03 14:31:13 +0530

    basics

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 //Assign types to data types
 let x = 10;
 let y = 'hello';
@@ -9,3 +9,45 @@ let z2 = 5;
 z2 = 'string';
 z2 = true;
 //any is a special data type in ts that can be replaced with any other data type value
+//Arrays in ts
+//in js let num=[1,2,3,'no'] as it is dynamic array it will allow any type of data
+//In ts we define types to arrays by using :number[] or :string[]
+let numArr = [1, 2, 3];
+let booArr = [true, false];
+let strArr = ['hello', 'world'];
+//but without declaring type it will be any type array
+let anyArr = [];
+anyArr.push(1);
+anyArr.push('hello'); //it can take any data type values
+//Tupes in ts : a fixed size arrray with different types
+let numStr = [1, 'hello'];
+//the postions cannot be interchanged and also not exceed the specified data types number and we cannot add or remove more or less than the specified number of data types
+let numStr1 = ['hello', 1, true];
+// let numStr2: [string, number, boolean] = ['hello', 1, true, 'no']; //throws error as we mentioned only 3 data types and 4 values
+// let numStr3: [string, number, boolean] = ['hello', 1]; //throws error as we mentioned  3 data types and has 2 values
+let numStr3 = ['hello', 1]; //we can use | to specify more than 1 data type
+let nums = numStr3[0];
+let numn = numStr3[1];
+var Size;
+(function (Size) {
+  Size['Small'] = 's';
+  Size['Medium'] = 'm';
+  Size['Large'] = 'l';
+})(Size || (Size = {}));
+console.log(0 /* Nums.One */, Size.Small); //Accessing using enum Name.value
+let num1 = 0; /* Nums.One */
+let size1 = Size.Small;
+console.log(size1, num1); //Accessing by assigning enum type to a variable
+//functions in ts
+//typescript will infer the return type of the function if not specified in below the type of function is number becuase return type is a number
+//parameters in ts are also inferred if not specified but parameter type must be specified
+function calculateTax(income) {
+  return 0;
+}
+//assign a type to a function it can also be void type if no return type or value is specified
+function calculateTax1(income = 2022) {
+  return 0; //it throws error is a string or any other is returned
+}
+calculateTax(10); //if passed more or less ts throws error but in js it does not care even if we pass more or less
+//useful config for  functions
+// noUnusedLocals: true,noImplicitReturns:true, noUnusedParameters:true
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,58 @@ let z2: any = 5;
 z2 = 'string';
 z2 = true;
 //any is a special data type in ts that can be replaced with any other data type value
+
+//Arrays in ts
+//in js let num=[1,2,3,'no'] as it is dynamic array it will allow any type of data
+//In ts we define types to arrays by using :number[] or :string[]
+let numArr: number[] = [1, 2, 3];
+let booArr: boolean[] = [true, false];
+let strArr: string[] = ['hello', 'world'];
+//but without declaring type it will be any type array
+let anyArr = [];
+anyArr.push(1);
+anyArr.push('hello'); //it can take any data type values
+
+//Tupes in ts : a fixed size arrray with different types
+let numStr: [number, string] = [1, 'hello'];
+//the postions cannot be interchanged and also not exceed the specified data types number and we cannot add or remove more or less than the specified number of data types
+let numStr1: [string, number, boolean] = ['hello', 1, true];
+// let numStr2: [string, number, boolean] = ['hello', 1, true, 'no']; //throws error as we mentioned only 3 data types and 4 values
+// let numStr3: [string, number, boolean] = ['hello', 1]; //throws error as we mentioned  3 data types and has 2 values
+let numStr3: [string | number, number] = ['hello', 1]; //we can use | to specify more than 1 data type
+let nums: string | number = numStr3[0];
+let numn: number = numStr3[1];
+
+//Enums in ts : used to define a set of named constants
+//we can assign values to enums in ts by using enum keyword
+//enums make it easier to work with a group of related values that are known at compile time.
+//instead of writing const small='s' and const medium='m' and const large='l' we can use enum keyword and assign values to enums
+const enum Nums {
+  One,
+  Two,
+  Three,
+} // by default enum starts from 0 and increments by 1 one=0,two=1,three=2 or one=1,two=2,three=3 if values are not specified
+enum Size {
+  Small = 's', //small=0
+  Medium = 'm', //medium=1
+  Large = 'l', //large=2
+}
+console.log(Nums.One, Size.Small); //Accessing using enum Name.value
+let num1: Nums = Nums.One;
+let size1: Size = Size.Small;
+console.log(size1, num1); //Accessing by assigning enum type to a variable
+
+//functions in ts
+//typescript will infer the return type of the function if not specified in below the type of function is number becuase return type is a number
+//parameters in ts are also inferred if not specified but parameter type must be specified
+function calculateTax(income: number) {
+  return 0;
+}
+//assign a type to a function it can also be void type if no return type or value is specified
+function calculateTax1(income: number = 2022): number {
+  return 0; //it throws error is a string or any other is returned
+}
+
+calculateTax(10); //if passed more or less ts throws error but in js it does not care even if we pass more or less
+//useful config for  functions
+// noUnusedLocals: true,noImplicitReturns:true, noUnusedParameters:true
